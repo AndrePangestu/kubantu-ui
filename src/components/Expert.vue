34da92eebfc4827284aa472493b05a63ec28bb08
Andre Pangestu <andre.pangestu22@gmail.com>
2017-12-09 18:36:52 +0700

    modif page expert and add library star rate

--- src/components/Expert.vue
+++ src/components/Expert.vue
@@ -1,16 +1,49 @@
 <template>
-  <div class="expert">
-    Expert
+  <div class="history">
+  <v-layout row>
+    <v-flex xs12 sm12 offset-sm3>
+
+        <v-list three-line>
+          <template v-for="item in items">
+            <v-subheader v-if="item.header" v-text="item.header"></v-subheader>
+            <v-divider v-else-if="item.divider" v-bind:inset="item.inset"></v-divider>
+            <v-list-tile avatar v-else v-bind:key="item.title" @click="">
+              <v-list-tile-avatar>
+                <img v-bind:src="item.avatar"/>
+              </v-list-tile-avatar>
+              <v-list-tile-content>
+                <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                <!-- <v-list-tile-sub-title v-html="item.rating"></v-list-tile-sub-title> -->
+                <star-rating :rating="4" :read-only="true" :increment="0.01" :star-size="45"></star-rating>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+        </v-list>
+
+    </v-flex>
+  </v-layout>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'expert',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js PWA'
+  export default {
+    name: 'expert',
+    data () {
+      return {
+        items: [
+          { header: 'Today' },
+          { avatar: '/static/doc-images/expert/bill-gates.jpg', title: 'Bill Gates', subtitle: "<span class='grey--text text--darken-2'>Job Title</span> — Web Master", rating: 'aa' },
+          { divider: true, inset: true },
+          { avatar: '/static/doc-images/expert/jack-ma.jpg', title: 'Jack Ma', subtitle: "<span class='grey--text text--darken-2'>Job Title</span> — Product Owner" },
+          { divider: true, inset: true },
+          { avatar: '/static/doc-images/expert/SteveJobsSquare.jpg', title: 'Steave Jobs', subtitle: "<span class='grey--text text--darken-2'>Job Title</span> — IOS Developer" },
+          { divider: true, inset: true },
+          { avatar: '/static/doc-images/expert/bill-gates.jpg', title: 'Bill', subtitle: "<span class='grey--text text--darken-2'>Job Title</span> — UI and UX Designer" },
+          { divider: true, inset: true },
+          { avatar: '/static/doc-images/expert/SteveJobsSquare.jpg', title: 'Steave', subtitle: "<span class='grey--text text--darken-2'>Job Title</span> — Frontend Developer" }
+        ]
+      }
     }
   }
-}
 </script>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import StarRating from 'vue-star-rating'
 // import App from './App'
 import App from './App'
 import router from './router'
 import('../node_modules/vuetify/dist/vuetify.min.css')
+import('../node_modules/vue-star-rating/dist/star-rating.min.js')
 import('./assets/css/custom.css')
 
 Vue.use(Vuetify)
+Vue.component('star-rating', StarRating)
 
 Vue.config.productionTip = false
 
